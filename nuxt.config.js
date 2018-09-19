029fd94524b847219e7313fa7e01547ba4996847
simochee <simochee@gmail.com>
2018-09-19 21:12:40 +0900

    🔧 Added tslint configures

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,47 @@
+const FriendlyErrorsWebpackPlugin = require('@nuxtjs/friendly-errors-webpack-plugin');
+
 module.exports = {
+  build: {
+    extend(config, { isServer }) {
+      const tsLoader = {
+        loader: 'ts-loader',
+        options: { appendTsSuffixTo: [/\.vue$/], transpileOnly: true },
+        exclude: [/vendor/, /\.nuxt/]
+      };
+
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(tsx?|vue)$/,
+        exclude: /(node_modules|\.nuxt)/,
+        use: [
+          {
+            loader: 'vue-tslint-loader',
+            options: {
+              emitErrors: true,
+              configFile: 'tslint.yml'
+            }
+          }
+        ]
+      });
+
+      config.module.rules.push({ test: /((client|server)\.js)|(\.tsx?)$/, ...tsLoader });
+      config.resolve.extensions.push('.ts');
+      config.module.rules.map((rule) => {
+        if (rule.loader === 'vue-loader') rule.options.loaders = { ts: tsLoader };
+        return rule;
+      });
+      config.plugins.push(new FriendlyErrorsWebpackPlugin());
+
+      if (isServer) config.externals = [];
+    }
+  },
   css: [
     'reset.css',
     '@/assets/styles/main.sass'
   ],
+  extensions: ['js', 'ts'],
   mode: 'spa',
   modules: [
-    '~/modules/typescript.js',
     ['nuxt-sass-resources-loader', '@/assets/styles/resources.sass']
   ],
   srcDir: 'src/'
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .container
+    h1 Hello, World.
+</template>
+
+<script lang="ts">
+</script>
