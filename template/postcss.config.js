ded23430c770c4809d37b79bde5dcd437486c241
simochee <simochee@gmail.com>
2018-11-02 19:51:09 +0900

    🔧 postcss, typescriptの設定を更新

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = () => ({
   plugins: [
+    require('stylelint')({
+      configFile: '.stylelintrc.yml'
+    }),
+    require('postcss-reporter')({
+      clearReportedMessages: true
+    }),
     require('autoprefixer')({
       browsers: ['last 2 versions']
     })
